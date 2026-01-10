b470f8d0aad73d8bf99f45003be73361db82b22a
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-01-10 16:02:50 +0000

    Mofify Values

---
+++ 01_Arrays/02_modify_values.js
@@ -0,0 +1,9 @@
+/* Access and Modify Values */
+
+let cricketers = ["Subhman","Rituraj","Ishan","Axar"];
+
+cricketers[2] = "Virat Kohli";
+
+console.log(cricketers)  // [ 'Subhman', 'Rituraj', 'Virat Kohli', 'Axar' ]
+
+console.log(cricketers[2]);  // Virat Kohli
