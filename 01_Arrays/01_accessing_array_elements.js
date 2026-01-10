69b57deebf3af3307462de7de703ae5a1a3b1a3d
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-01-10 16:01:52 +0000

    accessing array elements

---
+++ 01_Arrays/01_accessing_array_elements.js
@@ -0,0 +1,17 @@
+/* Array - Array is a group of variables stored in a single variable. */
+
+let cricketers = ["Subhman","Rituraj","Ishan","Axar"];
+
+console.log(cricketers);
+
+/* Accessing first element */
+
+console.log(cricketers[0]);
+
+/* Accessing last element */
+
+console.log(cricketers[cricketers.length-1]);
+
+/* Specific elements */
+
+console.log(cricketers[2]);
