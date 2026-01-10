21e4b62ec9279fcc9c61f7b7209a4c8680aeb37a
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-01-10 16:03:30 +0000

    length of the array

---
+++ 01_Arrays/03_array_length.js
@@ -0,0 +1,7 @@
+/* Caluclating length of the array */
+
+let cricketers = ["Subhman","Rituraj","Ishan","Axar"];
+
+console.log(cricketers.length);  // 4
+
+// length property return length of the existing array
