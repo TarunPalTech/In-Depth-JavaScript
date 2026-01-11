019af7735214ecc91a1bd5b6060341b48ec024e4
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-01-11 04:21:33 +0000

    Types

---
+++ 01_Arrays/07_types.js
@@ -0,0 +1,19 @@
+/* Array can store heterogeneous types of data... */
+
+let arr = [
+  "Tarun",
+  1,
+  35.4,
+  "Mohan",
+  null,
+  undefined,
+  { name: "Ravi Gangwar, age:23" },
+];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
