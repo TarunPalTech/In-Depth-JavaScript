6d461486fd1b4ac9b1991de3650d64c82fd73924
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-01-11 05:26:52 +0000

    slice vs splice

---
+++ 01_Arrays/10_slice_vs_splice.js
@@ -0,0 +1,17 @@
+/* 
+| Feature                | splice()         | slice()   |
+| ---------------------- | ---------------- | --------- |
+| Changes original array | ✅ Yes           | ❌ No   |
+| Can add elements       | ✅ Yes           | ❌ No   |
+| Can remove elements    | ✅ Yes           | ❌ No   |
+| Returns                | Removed elements |New array |
+
+splice() is just like a surgery (array is modified)
+slice() is just like a photocopy (array stays safe)
+
+
+🎯 Interview Line
+
+splice() modifies the original array by adding, removing, or replacing elements and returns the removed elements.
+
+*/
